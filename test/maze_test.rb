e5e516d2aeb23bea7648e9c05b324da2a76c6ea9
Matt Pindell <pindell.m@gmail.com>
2016-09-30 09:36:49 -0400

    Add test for size of maze

--- test/maze_test.rb
+++ test/maze_test.rb
@@ -1,4 +1,11 @@
 require 'minitest/autorun'
 
 class MazeTest < Minitest::Test
+
+  def test_maze_has_fixed_size
+    desired_size = 10
+    maze = Maze.new(desired_size)
+
+    assert_equal desired_size, maze.size
+  end
 end
